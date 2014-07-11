aa07d9fe13c492b15ac1b3dc572c24a923413ba3
mkihal <mkihal17@gmail.com>
2014-07-11 19:31:09 +0200

    Update app.js
    connection to the database and errors checking

--- app.js
+++ app.js
@@ -21,6 +21,22 @@ app.use(express.methodOverride());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(app.router);
 
+// MONGOOSE CONNECTION 
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
+var db = mongoose.connection;
+// MONGOOSE ERROR CHECK
+db.on('error', console.error.bind(console, ':connection error'));
+db.once('open', function callback(){
+     var kittySchema = mongoose.Schema({
+         name: 'String'
+     });
+     // compiling the schema into a model
+     var kitty = mongoose.model('kitty', kittySchema);
+     var minou = new kitty({name:'minou'});
+    console.log(minou.name+' said: the connection to the database is working like a charm chief :) ');
+    // Creating a method to our schema 'kittySchema'
+});
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
